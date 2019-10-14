d4626074a97acefb99dfc64b56ce3eba84e80030
Marissa Angell <maa.angell@gmail.com>
2019-10-14 09:39:11 -0600

    more progress

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,20 +1,12 @@
 $(".section2-col").mouseenter(function(){
-  $(this).addClass("col-7")
-
   $("#col2-1").removeClass("col-4")
   $("#col2-2").removeClass("col-4")
 
+  $("#col2-1").removeClass("col-7")
+  $("#col2-2").removeClass("col-7")
+
+  $(this).addClass("col-7")
+
   $("#col2-1").addClass("col-2")
   $("#col2-2").addClass("col-2")
-})
-
-$(".section2-col").mouseleave(function(){
-  $(this).removeClass("col-7")
-
-  $("#col2-1").removeClass("col-2")
-  $("#col2-2").removeClass("col-2")
-
-  $("#col2-1").addClass("col-4")
-  $("#col2-2").addClass("col-4")
-
 })
